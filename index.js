ac0ae05dbe72f4ae40dbab6cb4adceed880082d1
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-11-18 10:43:06 -0300

    test with axios

--- index.js
+++ index.js
@@ -78,19 +78,28 @@ async function callSendAPI(sender_psid, response) {
       "message": response
     };
 
-    const test = await request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
-      "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-      "method": "POST",
-      "json": request_body
-    }, (err, res, body) => {
-      if (!err) {
-        console.log('message sent!')
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }); 
-    console.log("testando retorno api do facebook", test)
+  const url = "https://graph.facebook.com/v2.6/me/messages";
+
+  const params = {
+    "access_token": process.env.PAGE_ACCESS_TOKEN
+  }
+
+  const response = await axios.post(url, request_body, { params })
+
+
+    // const test = await request({
+    //   "uri": "https://graph.facebook.com/v2.6/me/messages",
+    //   "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    //   "method": "POST",
+    //   "json": request_body
+    // }, (err, res, body) => {
+    //   if (!err) {
+    //     console.log('message sent!')
+    //   } else {
+    //     console.error("Unable to send message:" + err);
+    //   }
+    // }); 
+    console.log("testando retorno api do facebook", response)
 }
 
 app.get('/webhook', (req, res) => {
