3d93ed63aea589255375f8dd9e42607c23a09ca5
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-11-18 10:45:04 -0300

    require axios

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const axios = require('axios');
 const app = express().use(bodyParser.json());
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
